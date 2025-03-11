8d16a22a32c6bb67d1597604aa6ccd8cee04c385
imPepoDev <pedrogabrielofc@hotmail.com>
2025-03-11 16:19:36 -0300

    Obtendo um usuário por id

---
+++ src/main/java/kon/br/pepodev/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package kon.br.pepodev.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
